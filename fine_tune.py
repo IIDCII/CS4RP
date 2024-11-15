2dade3050031a8073a1049b5ffeb16c1c1bb11db
Dario Cline <darioclinemp3@gmail.com>
2024-11-15 16:35:30 +0000

    added code for general fine tuning

---
+++ fine_tune.py
@@ -0,0 +1,84 @@
+"""
+before running, run nvidia-smi in the terminal to see what gpu's are free in your cluster
+"""
+# setting up script
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "4,5"
+
+# imports
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, TrainingArguments, BitsAndBytesConfig
+from trl import SFTTrainer
+from peft import LoraConfig, get_peft_model
+
+
+# load model
+base_model_name = "llama-3.1-8B-Instruct"
+new_model_name = "llama-3.1-8B-Instruct-ML"
+
+# load tokenizer
+llama_tokenizer = AutoTokenizer.from_pretrained(base_model_name, trust_remote_code=True)
+llama_tokenizer.pad_token = llama_tokenizer.eos_token
+llama_tokenizer.padding_side = "right"
+
+# init model
+base_model = AutoModelForCausalLM.from_pretrained(
+    base_model_name,
+    torch_dtype=torch.float16,
+    device_map="auto",
+)
+base_model.config.use_cache = False
+base_model.config.pretraining_tp = 1
+
+# load data
+data_name = "reformatted data here"
+
+# Training Params
+train_params = TrainingArguments(
+    output_dir="./results_modified",
+    num_train_epochs=1,
+    per_device_train_batch_size=1,
+    gradient_accumulation_steps=1,
+    optim="paged_adamw_32bit",
+    save_steps=50,
+    logging_steps=50,
+    learning_rate=4e-5,
+    weight_decay=0.001,
+    fp16=False,
+    bf16=False,
+    max_grad_norm=0.3,
+    max_steps=-1,
+    warmup_ratio=0.03,
+    group_by_length=True,
+    lr_scheduler_type="constant",
+    report_to="tensorboard",
+)
+
+from peft import get_peft_model
+# LoRA Config
+# reduce rank r if you're running out of vram
+peft_parameters = LoraConfig(
+    r=4,
+    lora_alpha=8,
+    lora_dropout=0.1,
+    bias="none",
+    task_type="CAUSAL_LM"
+)
+model = get_peft_model(base_model, peft_parameters)
+model.print_trainable_parameters()
+
+# Trainer with LoRA configuration
+fine_tuning = SFTTrainer(
+    model=base_model,
+    train_dataset=data_name,
+    peft_config=peft_parameters,
+    dataset_text_field="text",
+    tokenizer=llama_tokenizer,
+    args=train_params
+)
+
+# Training
+fine_tuning.train()
+
+# Save Model
+fine_tuning.model.save_pretrained(new_model_name)
